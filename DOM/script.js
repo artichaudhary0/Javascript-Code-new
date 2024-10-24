1a110143b128d7e7ddac9bf6a67c93c4d6abc21c
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2024-10-24 17:12:22 +0530

    InnerHtml, innerText, textContent

--- DOM/script.js
+++ DOM/script.js
@@ -1,38 +1,47 @@
-// manually walking over the whole html 
-let body  = document.body.children[4].children[1]
+// manually walking over the whole html
+let body = document.body.children[4].children[1];
 
-let th = document.getElementsByTagName("th") // HTML COLLECTION
-let thByClass = document.getElementsByClassName("one") // HTML COLLECTION
-let thByID = document.getElementById("recordList") // Element
+let th = document.getElementsByTagName("th"); // HTML COLLECTION
+let thByClass = document.getElementsByClassName("one"); // HTML COLLECTION
+let thByID = document.getElementById("recordList"); // Element
 
-let thByQuerySelectorTag = document.querySelector("h1") // NodeElement first occurance of tag parameter you passed
-let thByQuerySelectorId = document.querySelector("#recordList") // NodeElement => first occurance of Id parameter you passed
-let thByQuerySelectorClass = document.querySelector(".one") // NodeElement => first occurance of class parameter you passed
-let thByQuerySelectorAttribute = document.querySelector("[hello]") // NodeElement => first occurance of attributes parameter you passed
+let thByQuerySelectorTag = document.querySelector("h1"); // NodeElement first occurance of tag parameter you passed
+let thByQuerySelectorId = document.querySelector("#recordList"); // NodeElement => first occurance of Id parameter you passed
+// let thByQuerySelectorClass = document.querySelector(".one"); // NodeElement => first occurance of class parameter you passed
+let thByQuerySelectorAttribute = document.querySelector("[hello]"); // NodeElement => first occurance of attributes parameter you passed
 
-
-let thByQuerySelectorAllTag = document.querySelectorAll("h1") // NodeList All the occurance of tag parameter you passed
-let thByQuerySelectorAllId = document.querySelectorAll("#recordList") // NodeList => All the occurance of Id parameter you passed
+let thByQuerySelectorAllTag = document.querySelectorAll("h1"); // NodeList All the occurance of tag parameter you passed
+let thByQuerySelectorAllId = document.querySelectorAll("#recordList"); // NodeList => All the occurance of Id parameter you passed
 // let thByQuerySelectorAllClass = document.querySelectorAll(".one") // NodeList => All the occurance class parameter you passed
-let thByQuerySelectorAllAttribute = document.querySelectorAll("[hello]") // NodeList => All the occurance of attributes parameter you passed
+let thByQuerySelectorAllAttribute = document.querySelectorAll("[hello]"); // NodeList => All the occurance of attributes parameter you passed
 
-// class = one 
-let thByQuerySelectorAllClass = document.querySelectorAll(".one")
+// class = one
+let thByQuerySelectorAllClass = document.querySelectorAll(".one");
 
-for(let i = 0 ; i < thByQuerySelectorAllClass.length ; i++)
-{
-    thByQuerySelectorAllClass[i].innerHTML = `<table>
-      <thead>
-        <tr>
-          <th class="one" hello = "hello world" style = "color: white;">Name</th>
-          <th class="one">Details</th>
-          <th class="one">Date</th>
-          <th class="one">Actions</th>
-        </tr>
-      </thead>
+let mainHeading = document.querySelector("h1");
 
-    </table>`
+let removeHelloArti = document.querySelectorAll("h1")[1];
 
+removeHelloArti.style.display = "none";
+
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => {
+  mainHeading.style.backgroundColor = "teal";
+  mainHeading.style.color = "white";
+  mainHeading.style.borderRadius = "50px";
+  mainHeading.style.padding = "12px";
+  mainHeading.innerText = "Project made by Arti";
+});
+
+mainHeading.innerHTML = `<h1>INNER <span style = "color : red;">HTML</span></h1>`; // append this to mainHeading
+mainHeading.innerText = `<h1>INNER <span style = "color : red;">HTML</span></h1>`; // treat this as a string
+mainHeading.textContent = `<h1>INNER <span style = "color : red;">HTML</span></h1>`; // treat this as a string
+
+let tr = document.getElementsByTagName("tr");
+
+console.log(thByQuerySelectorAllClass);
+
+for (let i = 0; i < thByQuerySelectorAllClass.length; i++) {
+  thByQuerySelectorAllClass[i].innerHTML = `<h4>INNER <span style = "color : red;">HTML</span></h4>`;
 }
-
-
