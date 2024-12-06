0954b0080184f89ac0d3cc36af7d0871de24565d
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2024-12-06 16:49:37 +0530

    Ecommerce 2 done

--- Ecommerce2/script.js
+++ Ecommerce2/script.js
@@ -1,43 +1,4 @@
-const smapleProducts = [
-  {
-    name: "Product 1",
-    description: "description of project 1",
-    image: "https://placehold.co/300x300",
-  },
-  {
-    name: "Product 2",
-    description: "description of project 2",
-    image: "https://placehold.co/300x300",
-  },
-  {
-    name: "Product 3",
-    description: "description of project 3",
-    image: "https://placehold.co/300x300",
-  },
-  {
-    name: "Product 4",
-    description: "description of project 4",
-    image: "https://placehold.co/300x300",
-  },
-  {
-    name: "Product 5",
-    description: "description of project 5",
-    image: "https://placehold.co/300x300",
-  },
-  {
-    name: "Product 6",
-    description: "description of project 6",
-    image: "https://placehold.co/300x300",
-  },
-  {
-    name: "Product 7",
-    description: "description of project 7",
-    image: "https://placehold.co/300x300",
-  },
-];
-
 // class for handling navigation.
-
 class NavBar {
   constructor() {
     this.render();
@@ -76,7 +37,7 @@ class Products {
             <img src="${product.image}" alt="${product.name}">
             <h3>${product.name}</h3>
             <p>${product.description}</p>
-            <button onclick="cart.addTocart("${product.name}","${product.image}","${product.description}")>Add to cart</button>
+            <button onclick="cart.addTocart('${product.name}','${product.image}','${product.description}')">Add to cart</button>
         </div>
         `
       )
@@ -98,6 +59,101 @@ class Footer {
   }
 }
 
-new Footer();
-new NavBar();
-new Products(smapleProducts, []);
+class Cart {
+  constructor() {
+    this.items = JSON.parse(localStorage.getItem("cart")) || [];
+    this.updateCartDisplay();
+  }
+
+  addTocart(name, image, description) {
+    this.items.push({ name, image, description });
+    console.log(this.items);
+
+    localStorage.setItem("cart", JSON.stringify(this.items));
+    this.updateCartDisplay();
+
+    alert(`${name}  has been added to the cart!`);
+  }
+
+  updateCartDisplay() {
+    const navbar = new NavBar();
+    navbar.updateCartCount(this.items.length);
+  }
+  displayCartItems() {
+    const cartItemsContainer = document.getElementById("cart-items");
+    console.log(cartItemsContainer);
+
+    if (this.items.length === 0) {
+      cartItemsContainer.innerHTML = `<p> Your cart is empty</p>`;
+    } else {
+      cartItemsContainer.innerHTML = this.items
+        .map(
+          (item) => `
+        <div class="cart-item">
+        <h4>${item.name}</h4>
+        <img src="${item.image}" alt="${item.description}" width="50">
+        <button onclick="cart.removeFromCart('${item.name}')">Remove</button>
+        </div>
+        `
+        )
+        .join("");
+    }
+  }
+
+  removeFromCart(name) {
+    this.items = this.items.filter((item) => item.name !== name);
+    localStorage.setItem("cart", JSON.stringify(this.items));
+    this.displayCartItems();
+    this.updateCartDisplay();
+  }
+}
+const cart = new Cart();
+const smapleProducts = [
+  {
+    name: "Product 1",
+    description: "description of project 1",
+    image: "https://placehold.co/300x300",
+  },
+  {
+    name: "Product 2",
+    description: "description of project 2",
+    image: "https://placehold.co/300x300",
+  },
+  {
+    name: "Product 3",
+    description: "description of project 3",
+    image: "https://placehold.co/300x300",
+  },
+  {
+    name: "Product 4",
+    description: "description of project 4",
+    image: "https://placehold.co/300x300",
+  },
+  {
+    name: "Product 5",
+    description: "description of project 5",
+    image: "https://placehold.co/300x300",
+  },
+  {
+    name: "Product 6",
+    description: "description of project 6",
+    image: "https://placehold.co/300x300",
+  },
+  {
+    name: "Product 7",
+    description: "description of project 7",
+    image: "https://placehold.co/300x300",
+  },
+];
+
+if (document.getElementById("main-content")) {
+  new NavBar();
+
+  new Products(smapleProducts, cart);
+}
+
+if (document.getElementById("cart-items")) {
+  new NavBar();
+
+  cart.displayCartItems();
+}
