7a435dde9e1de8b227cb3bbfb0e62bd763617caf
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2024-12-16 16:47:34 +0530

    fetch

--- fetch/script.js
+++ fetch/script.js
@@ -1,69 +1,32 @@
-var button = document.getElementById("fetch");
+let h = document.getElementById("fatcs");
 
-button.addEventListener("click", buttonClickHandle);
+var fetchData = document.getElementById("btn");
 
-//  "data": {
-//   "name": "test",
-//   "salary": "123",
-//   "age": "23",
-//   "id": 25
-// }
+let url = "https://cat-fact.herokuapp.com/facts";
 
-// function buttonClickHandle() {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open("POST", "https://dummy.restapiexample.com/api/v1/create", true);
+// var getFacts = async () => {
+//   let respose = await fetch(url);
 
-//   xhr.getResponseHeader("Content-type", "application-json");
+//   let responseData = await respose.json(); // CONVERSION
 
-//   params = {
-//     name: "Arti",
-//     salary: "123",
-//     age: "24",
-//   };
+//   responseData.forEach((element) => {
+//     h.innerText = element["text"];
+//   });
 
-//   xhr.send(params);
-// }
+//   console.log(responseData);
+// };
 
-function buttonClickHandle() {
-  // instance of XMLHTTPREQUEST
-  const xhr = new XMLHttpRequest();
+// getFacts();
 
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
-
-  xhr.onprogress = function () {};
-
-  xhr.onload = function () {
-    if (this.status === 200) {
-      let response = JSON.parse(this.responseText); // JSON
-      displayData(response);
-      // conver
-      return response;
-    } else {
-      console.log(this.status);
-    }
-  };
-
-  xhr.send();
+function getFacts() {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((element) => {
+        h.innerText = element["text"];
+      });
+    });
 }
-
-function displayData(response) {
-  const container = document.getElementById("data-container");
-  container.innerHTML = "";
-  response.forEach((user) => {
-    const userDiv = document.createElement("div");
-
-    userDiv.className = "arti";
-
-    userDiv.innerHTML = `
-      <h3>${user.id}</h3>
-      <h4>${user["address"]["street"]}</h4>
-      <h4>${user["company"]["name"]}</h4>
-    `;
-
-    container.appendChild(userDiv);
-  });
-}
-
-console.log(document.querySelector(".arti"));
-
-// let data = buttonClickHandle();
+fetchData.addEventListener("click", getFacts);
