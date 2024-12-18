369c1058f3361dd119445dace59add4736b236cb
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2024-12-18 17:19:54 +0530

    array built in

---
+++ Array/script.js
@@ -0,0 +1,40 @@
+// Array With Functions (map ,filter, foreach, indexof, findindex ,includes, every, some)
+// Array With Functions Push ,Pop, shift, unshifts,
+
+let array = [1, 2, 3, 4, 2, 2, 5, 98, 67, 7];
+
+// add element last
+// array.push(56);
+
+// console.log(array);
+
+// array.pop(56);
+// console.log(array);
+
+// array.shift(56);
+
+// console.log(array);
+
+// array.unshift(234);
+
+// console.log(array);
+
+// array.map((elem) => elem * 2);
+
+// console.log(array);
+
+let array2 = array.filter((value) => value > 3);
+
+// console.log(array2);
+
+// array.forEach((value) => console.log(value * 2));
+
+let search = array.indexOf(98, 8);
+// console.log(search);
+
+// let i = array.findIndex((value) => value > 5);
+// console.log(i);
+
+// let i =array.includes(98,8)
+
+// console.log(i)
